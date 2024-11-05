cfa6a8ecb7e205c26b9019228bc3ffdbcc331400
dxdiag333 <dmmnpdclid12tf45@gmail.com>
2024-11-05 12:19:04 +0700

    Update BelajarWidget.dart

--- BelajarWidget.dart
+++ BelajarWidget.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 void main() => runApp(const MyApp());
@@ -7,23 +9,95 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          title: const Text('My Home Page')
-        ),
         body: Center(
           child: Builder(
             builder: (context) {
-              return Column(
-                children: [
-                  const Text("hello world"),
-                  const SizedBox(height: 20),
-                  ElevatedButton(
-                    onPressed: () {
-                      print("click");
-                    },
-                    child: const Text("a button"),
-                  ),
-                ],
+              return Padding(
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 100,
+                ),
+                child: Column(
+                  children: [
+                    Container(
+                      margin: EdgeInsets.symmetric(vertical: 20),
+                      alignment: Alignment.center,
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text(
+                            "Flutter",
+                            style: TextStyle(
+                              fontSize: 60,
+                            )
+                          )
+                        ]
+                      )
+                    ),
+                    Container(
+                      margin: EdgeInsets.symmetric(vertical: 10),
+                      child: TextField(
+                        decoration: InputDecoration(
+                          border: OutlineInputBorder(
+                            borderSide: BorderSide(
+                              color: Color.fromARGB(0, 0, 0, 0),
+                              width: 1.0,
+                              style: BorderStyle.solid
+                            )
+                          ),
+                          hintText: "Email"
+                        )
+                      ),
+                    ),
+                    Container(
+                      margin: EdgeInsets.symmetric(vertical: 10),
+                      child: TextField(
+                        decoration: InputDecoration(
+                          border: OutlineInputBorder(
+                            borderSide: BorderSide(
+                              color: Color.fromARGB(0, 0, 0, 0),
+                              width: 1.0,
+                              style: BorderStyle.solid
+                            )
+                          ),
+                          hintText: "Password"
+                        )
+                      ),
+                    ),
+                    Container(
+                      margin: EdgeInsets.symmetric(vertical: 10),
+                      child: Text(
+                        "Forgot password",
+                        style: TextStyle(
+                          color: Color.fromARGB(255, 34, 150, 243)
+                        ),
+                      )
+                    ),
+                    SizedBox(
+                      width: 150,
+                      height: 50,
+                      child: ElevatedButton(
+                        style: ButtonStyle(
+                          backgroundColor: WidgetStatePropertyAll(Color.fromARGB(255, 34, 150, 243)),
+                        ),
+                        onPressed: () => true,
+                        child: Text(
+                          "Login",
+                          style: TextStyle(
+                            color: Color.fromARGB(255, 255, 255, 255)
+                          )
+                        )
+                      )
+                    ),
+                    Container(
+                      margin: EdgeInsets.only(
+                        top: 200
+                      ),
+                      child: Text(
+                        "New User? Create Account"
+                      )
+                    )
+                  ],
+                )
               );
             },
           ),
